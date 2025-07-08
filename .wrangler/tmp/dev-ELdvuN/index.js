87a1781f78b15605931fb3548a41bb2ad7dd4ba6
Quentin Cody <QuentinCody@gmail.com>
2025-07-08 05:00:05 -0400

    Implement middleware loading and insertion facade; update wrangler configuration with migration tags for SQLite classes

--- .wrangler/tmp/dev-ELdvuN/index.js
+++ .wrangler/tmp/dev-ELdvuN/index.js
@@ -8428,7 +8428,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env2, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-ktNslV/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-s2ni9C/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -8460,7 +8460,7 @@ function __facade_invoke__(request, env2, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-ktNslV/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-s2ni9C/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
